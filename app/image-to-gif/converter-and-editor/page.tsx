a072d341ed3cd32c044e69dbe8e1ce8d919eb7bb
heiner <heiner.giehl@yahoo.com>
2024-04-05 12:51:42 +0200

    vercel speed insights useRouter bug, unused import

--- app/image-to-gif/converter-and-editor/page.tsx
+++ app/image-to-gif/converter-and-editor/page.tsx
@@ -1,6 +1,16 @@
 "use client"
 import React from "react"
 import { EditorWithStore } from "@/components/video-to-gif/Editor"
+import dynamic from "next/dynamic"
+const DynmaicEditor = dynamic(
+  () =>
+    import("@/components/video-to-gif/Editor").then(
+      (mod) => mod.EditorWithStore
+    ),
+  {
+    ssr: false,
+  }
+)
 export default function ConverterAndEditor() {
-  return <EditorWithStore />
+  return <DynmaicEditor />
 }
