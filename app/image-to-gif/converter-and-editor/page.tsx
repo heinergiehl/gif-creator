f94a9bd861653218f8938cc6f1cf8c338d292967
heiner <heiner.giehl@yahoo.com>
2024-04-09 08:47:10 +0200

    favicon

--- app/image-to-gif/converter-and-editor/page.tsx
+++ app/image-to-gif/converter-and-editor/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 import React from "react"
-import { EditorWithStore } from "@/components/video-to-gif/Editor"
 import dynamic from "next/dynamic"
 const DynmaicEditor = dynamic(
-  () =>
-    import("@/components/video-to-gif/Editor").then(
-      (mod) => mod.EditorWithStore
-    ),
+  () => import("@/components/video-to-gif/Editor"),
   {
     ssr: false,
   }
