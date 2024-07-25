dffb5a96bf4fb486c98a716073bb320d129ff5a2
heiner <heiner.giehl@yahoo.com>
2024-07-25 13:30:57 +0200

    about to fix the mobile view of editor, almost done <3

--- components/video-to-gif/Editor.tsx
+++ components/video-to-gif/Editor.tsx
@@ -287,7 +287,7 @@ const Editor = React.memo(
           <div className=" flex w-screen flex-col  items-center justify-center md:w-full">
             <EditResource />
             <div
-              className="flex h-[calc(100dvh-50px)] w-screen  flex-col items-center justify-center md:w-full"
+              className="flex h-[calc(100svh-50px)] w-screen  flex-col items-center justify-center md:w-full"
               id="editor-container"
             >
               <CustomAlertDialog />
@@ -295,7 +295,7 @@ const Editor = React.memo(
                 className="relative  flex h-full  w-full flex-col items-start justify-start  md:h-[calc(100dvh-50px)] md:w-full"
                 draggable="false"
               >
-                <ScrollArea className="m-auto  flex h-[calc(100dvh-0px)] flex-col items-center justify-center  gap-y-2 rounded-none md:h-full md:flex-row">
+                <ScrollArea className="m-auto  flex h-[calc(100svh-0px)] flex-col items-center justify-center  gap-y-2 rounded-none md:h-full md:flex-row">
                   <div className="flex h-full flex-col items-center justify-center gap-4  md:flex-row">
                     <div className="flex flex-row items-center justify-center gap-4  md:flex-col">
                       <div className="flex flex-row items-center  justify-center gap-2 md:flex-col">
