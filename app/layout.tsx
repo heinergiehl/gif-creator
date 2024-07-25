6e95241508bc975801e9ba2f6832b011c2da2f28
heiner <heiner.giehl@yahoo.com>
2024-07-25 02:34:00 +0200

    about to fix the mobile view of editor, almost done <3

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,15 @@ import dynamic from 'next/dynamic';
 import { ThemeProvider } from '@/app/theme-provider';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
+import { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] });
 const CookieBanner = dynamic(() => import('@/app/components/consent/CookieBanner'), {
   ssr: false,
 });
+export const metaData: Metadata = {
+  // meta initial zoom
+  content: 'width=device-width, initial-scale=1',
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Suspense>
         <GoogleAnalytics GA_MEASUREMENT_ID="G-8M37TENBJS" />
       </Suspense>
